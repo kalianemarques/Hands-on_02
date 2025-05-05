37ba5c0fe4b46b95631c69ad51b1b17192ce6b4d
Kaliane Marques <kalianemarques@outlook.com>
2025-05-05 00:25:08 -0300

    estrutura inicial

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+// require('dotenv').config();
+
+const app = express();
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+const apiRoutes = require('./src/routes/apiRoutes');
+app.use('/api', apiRoutes);
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
---
+++ src/routes/apiRoutes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+
+// Importa depois a função executePython do arquivo utils/executePython.js
+// Adicionar as rotas 
+
+module.exports = router;
