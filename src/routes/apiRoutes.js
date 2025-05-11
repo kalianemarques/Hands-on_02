669147c90c32654d663090fc6e1217f60f13c645
Kaliane Marques <kalianemarques@outlook.com>
2025-05-11 20:34:00 -0300

    adição das rotas e do arquivo responsavel por chamar o script py

--- src/routes/apiRoutes.js
+++ src/routes/apiRoutes.js
@@ -1,7 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const { executePython } = require('../utils/executePython');
 
-// Importa depois a função executePython do arquivo utils/executePython.js
-// Adicionar as rotas 
+router.post('/channel', (req, res) => {
+    const { m, n, sigma } = req.body; 
+    if (!m || !n || !sigma) {
+        return res.status(400).json({ error: 'Parâmetros "m", "n" e "sigma" são obrigatórios.' });
+    }
+    executePython('TriggerGenerate.py', [m, n, sigma], res);
+});
+
+
+router.post('/estimative', (req, res) => {
+    const { windowSize } = req.body; 
+    if (! windowSize) {
+        return res.status(400).json({ error: 'Parâmetro "windowSize" é  obrigatório.' });
+    }
+    executePython('TriggerEstimate.py', [windowSize], res); 
+});
 
 module.exports = router;
